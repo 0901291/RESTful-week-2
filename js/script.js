232a85965a80476d1605f9e97ceb106db36ced20
ianwensink <ianwensink@gmail.com>
2015-11-29 10:59:36 +0100

    Cleanup #2 + added functionality
    Added
    - Date is shown in note
    - Author is shown in note
    - Title of note is shown in system message

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 $(initApp);
 
 function initApp() {
+    $(".container").on("click", ".note", getAuthor);
     $(".container").on("click", "#notes .edit-button", initEditNote);
     $(".container").on("click", "#notes .delete-button", deleteNote);
     $(".container").on("click", "#note-form .panel-heading", function() {
@@ -8,9 +9,7 @@ function initApp() {
     });
     $(".container").on("click", "#save-note", initSaveNote);
     $(".container").on("click", "#cancel-note", closeNoteForm);
-    requestNote("getNote", "GET", "", function (output) {
-        printNotes(output);
-    });
+    requestNote("getNote", "GET", "", printNotes);
 }
 
 function printNotes(data) {
@@ -24,12 +23,14 @@ function printNotes(data) {
 
 function formatNote(note) {
     return "<li href='#' data-id='" + note.id + "' data-toggle='collapse' data-target='#note-" + note.id + "' data-parent='.container' class='note collapsed list-group-item'>" +
-                "<span class='note-title'>" + note.title + "</span>" +
+                "<span class='note-title'>" + truncateText(note.title, 30) + "</span>" +
                 "<span class='note-intro'>" + truncateText(note.body, 50) + "</span>" +
             "</li>" +
             "<div class='sublinks collapse' id='note-" + note.id + "'>" +
                 "<div class='note-content list-group-item'>" +
-                    "<p>" + note.body + "</p>" +
+                    "<h3>" + note.title + "</h3>" +
+                    "<pre>" + note.body + "</pre>" +
+                    "<p class='author'>By </p>" +
                     "<button type='button' class='edit-button btn btn-primary'>Edit</button><button type='button' class='delete-button btn btn-danger'>Delete</button>" +
                 "</div>" +
             "</div>"
@@ -41,11 +42,11 @@ function initEditNote(e) {
     requestNote("getNote", "GET", id.substring(5, id.length), fillNoteForm);
 }
 
-function fillNoteForm(output) {
-    $("#note-id").val(output.id);
-    $("#input-title").val(output.title);
-    $("#input-body").val(output.body);
-    $("#input-author").val(output.author);
+function fillNoteForm(note) {
+    $("#note-id").val(note.id);
+    $("#input-title").val(note.title);
+    $("#input-body").val(note.body);
+    $("#input-author").val(note.author);
     $("body").animate({ scrollTop: 0 });
     $("#note-form .panel-body").removeClass("closed");
 }
@@ -65,17 +66,17 @@ function initSaveNote() {
         extendNote({}, "createNote", function (output) {
             $("#notes").append(formatNote(output));
             closeNoteForm();
-            showMessage("<strong>Saved!</strong> Your note is successfully saved.");
+            showMessage("<strong>Saved!</strong> Your note <strong>\"" + truncateText(output.title, 30) + "\"</strong> is successfully saved.");
         });
     } else if ($.isNumeric(id)) {
-        requestNote("getNote", "GET", id, function (output) {
-            extendNote(output, "updateNote", function (output) {
-                $("#notes .note[data-id=" + output.id + "]").remove();
-                $("#notes #note-" + output.id).addClass("to-remove").after(formatNote(output));
+        requestNote("getNote", "GET", id, function (note) {
+            extendNote(note, "updateNote", function (note) {
+                $("#notes .note[data-id=" + note.id + "]").remove();
+                $("#notes #note-" + note.id).addClass("to-remove").after(formatNote(note));
                 $(".to-remove").remove();
+                closeNoteForm();
+                showMessage("<strong>Saved!</strong> Your note <strong>\"" + truncateText(note.title, 30) + "\"</strong> is successfully saved.");
             });
-            closeNoteForm();
-            showMessage("<strong>Saved!</strong> Your note is successfully saved.");
         });
     }
 }
@@ -94,10 +95,18 @@ function deleteNote(e) {
     var id = $(e.currentTarget).parent().parent().attr("id");
     id = id.substring(5, id.length);
     requestNote("deleteNote", "POST", id, function (output) {
-        $("#notes .note[data-id=" + id + "]").remove();
-        $("#notes #note-" + id).remove();
+        $(".note[data-id=" + id + "]").remove();
+        var title = $("#note-" + id).find("h3").text();
+        $("#note-" + id).remove();
         $("body").animate({ scrollTop: 0 });
-        showMessage("<strong>Deleted!</strong> Your note is successfully deleted.");
+        showMessage("<strong>Deleted!</strong> Your note <strong>\"" + truncateText(title, 30) + "\"</strong> is successfully deleted.");
+    });
+}
+
+function getAuthor(e) {
+    var id = $(e.currentTarget).data("id")
+    requestNote("getNote", "GET", id, function (note) {
+        $("#note-" + id).find(".author").text("By " + (note.author.length > 0 ? note.author : "-") + " on " + note.date);
     });
 }
 
